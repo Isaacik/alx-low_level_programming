53b250545e8ab4a4acb4b94bf5adf6fa8a2570f6
Isaacik <isaacikhaiduwor1@gmail.com>
2022-03-22 07:32:08 -0700

    task 5: Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,10 @@
 /* Program to print a statment using printf*/
 #include <stdio.h>
 
+/**
+ *main - entry point
+ *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
