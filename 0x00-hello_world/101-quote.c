c9ccee8f6769e10d85b906d5165057c0cd016435
Isaacik <isaacikhaiduwor1@gmail.com>
2022-03-22 08:42:00 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+
+/* Program to print a statment */
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+
+}
+
